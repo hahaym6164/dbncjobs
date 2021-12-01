d9fced7c1f2042e363685753ed4a9d563f0e0b5f
unknown <616477730@qq.com>
2021-12-01 12:22:24 -0500

    finished

--- question5/src/App.js
+++ question5/src/App.js
@@ -2,17 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import axios from "axios"
 import react, { useState } from 'react'
-
+import SearchCity from './search/searchCIty';
 const App = () => {
 
   return (
     <div className="App">
       <div className="container">
-        {cities.map(i => (
-          <div>
-            {i} test
-          </div>
-        ))}
+        <SearchCity />
       </div>
     </div>
   );
--- question5/src/search/searchCIty.js
+++ question5/src/search/searchCIty.js
@@ -1,32 +1,88 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import './style.css'
-const url = "https://www.metaweather.com/api/location/search/?query="
-
+const idUrl = "https://www.metaweather.com/api/location/search/?query="
+const forecastUrl = "https://www.metaweather.com/api/location/"
 export default class SearchCity extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cities: []
+            cities: [],
+            noResult: false
         }
 
-    }
-    componentDidMount() {
-        axios.get(url + e, {
-        }).then(res => {
-            // this.setState
-        })
 
     }
+    updateCities(e) {
+        axios.get(idUrl + e.target.value).then(res => {
+            let ids = []
+            res.data.map(city => {
+                ids.push({
+                    name: city.title,
+                    id: city.woeid
+
+                })
+            })
+            ids.map(i => {
+                axios.get(forecastUrl + i.id).then(res1 => {
+                    res1.data.title = i.name
+                    this.state.cities.push(res1.data)
+
+                    this.setState({
+                        cities: this.state.cities
+                    })
+                }).catch(err => console.log(err))
+            })
+            if (res.data.length == 0) {
+                this.setState({
+                    noResult: true
+                })
+            }
 
 
+        }).catch(err => console.log(err))
+    }
+
+    searchCitiesList(e) {
+        // run --disable-web-security --disable-gpu --user-data-dir=~/chromeTemp to Avoid CORS
+
+        // if user hit enter update the cities list
+
+        if (e.key === "Enter" && e.target.value) {
+
+            this.setState({
+                noResult: false,
+                cities: []
+            }, this.updateCities(e))
+
+        }
+    }
 
     render() {
-
-        let { studentList } = this.state;
+        let { cities, noResult } = this.state
         return (
             <div>
+                <h3>Hit Enter to search the city</h3>
+                <input type="text" placeholder="Enter your city" className="search-bar" onKeyUp={this.searchCitiesList.bind(this)} />
+                {noResult ? <h3>No result based on your search</h3> : ''}
+                {cities.map(i => {
+
+                    return (
+                        <div key={i.woeid}>
+                            <h1>City:{i.title}</h1>
+                            {i.consolidated_weather.map(j => {
+                                return (
+                                    <div className="city" key={j.id}>
+                                        <div>Weather: {j.weather_state_name}  </div>
+                                        <div> Date: {j.applicable_date}  </div>
+                                    </div>
+                                )
+                            }
+                            )}
+                        </div>
+                    )
+                }
+                )}
             </div>
         );
     }
